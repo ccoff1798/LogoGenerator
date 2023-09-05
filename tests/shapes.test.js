3fcea96c9a11b8c2067cfe0bcf2fed79c03ab1ed
Cody <ccoff1798@gmail.com>
2023-09-05 15:36:03 -0600

    I am so confused, need youtube videos to get logo.svg onto html

---
+++ tests/shapes.test.js
@@ -0,0 +1,26 @@
+const Generator = require('../lib/shapes.js'); // Adjust the path to match your project structure
+const inquirer = require('inquirer');
+
+jest.mock('inquirer');
+
+describe('Generator', () => {
+    test('generateLogo should generate an SVG file', async () => {
+        inquirer.prompt = jest.fn()
+            .mockResolvedValueOnce({
+                text: 'ABC',
+                textColor: 'blue',
+                shape: 'circle',
+                shapeColor: 'red'
+            });
+        const consoleLogMock = jest.spyOn(console, 'log');
+        consoleLogMock.mockImplementation(() => {});
+
+        const generator = new Generator();
+
+        await generator.generateLogo();
+
+        expect(consoleLogMock).toHaveBeenCalledWith('Generated logo.svg');
+        
+        consoleLogMock.mockRestore();
+    });
+});
